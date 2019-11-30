16ea2f3ceef4910fd57b5fa58dbfdcc006070d62
Bernadette Davis <bsd231@nyu.edu>
2019-11-30 16:59:21 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, length: { minimum: 1 }
+  validates :content, length: { minimum: 100 }
   validates :category, length: { minimum: 1 }
 end
