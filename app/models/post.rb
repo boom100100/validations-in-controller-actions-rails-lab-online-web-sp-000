50ed0a11f9f0c19a18eb596b93f5f1e4ae7dda4f
Bernadette Davis <bsd231@nyu.edu>
2019-11-30 17:39:18 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,5 @@
 class Post < ActiveRecord::Base
-  include ActiveModel::Validations
-  validates_with CategoryValidator
-
   validates :title, presence: true
   validates :content, length: { minimum: 100 }
-  #validates :category, length: { minimum: 1 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
